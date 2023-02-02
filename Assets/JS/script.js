4169e835005948d4a9ee39bd1a1d8eb255355bd6
Patrick Ameglio <pameglio101@yahoo.com>
2023-02-02 14:09:26 -0500

    making sure i saved the extra pushd through the deleted extra divs i had

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -8,4 +8,42 @@ $(document).ready ($(function () {
       var hour = $(this).parent().attr("id");
       var hourlyTask = $(this).siblings(".description").val();
       localStorage.setItem(hour,hourlyTask);
-  })}))
+
+  })
+
+// Function to check current time of day and change text block styles based on time
+function trackTime() {   
+  $(".time-block").each(function(){
+      var currentHour = parseInt($(this).attr("id").split("hour-")[1]);
+
+      if (currentHour === dayjs().hour()){
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");      
+      }
+      if (currentHour < dayjs().hour()){
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");      
+      }
+      if (currentHour > dayjs().hour()){
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }
+
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+})}
+
+trackTime();
+
+}));
